2e4f985b9b8641de06b12f951094dd885c4fe0f4
Anya <annagapyuk@gmail.com>
2021-09-22 13:59:48 -0400

    added state

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./components/CardList";
 import { robots } from "./robots";
 import SearchBox from "./components/SearchBox";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+// State >> props
+// state is an object
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
