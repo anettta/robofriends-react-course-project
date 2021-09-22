505ab1c3ec89064e016fc3045d5379c890527b30
Anya <annagapyuk@gmail.com>
2021-09-22 13:38:35 -0400

    added SearchBox

--- robofriends/src/components/SearchBox.js
+++ robofriends/src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Searchbox = () => {
   return (
-    <div className="pa2">
+    <div className="pa3">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
