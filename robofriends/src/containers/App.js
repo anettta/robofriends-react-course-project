0b0ab8e39755912bf91467490e7e04e0062e2276
Anya <annagapyuk@gmail.com>
2021-09-23 00:06:13 -0400

    connected to redux

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoudary from "../components/ErrorBoundary";
 import "./App.css";
 
+import { setSearchField } from "../actions.js";
+
 // State >> props
 // state is an object
+// smart components = containers
 // smart components have state and a class
 // hooks are a way to avoid classes
 // hooks allow us to use functional components
 // redux toolkit
 
-function App() {
+const App = ({ store }) => {
   // two separate states
   const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState("");
+
+  // const [searchfield, setSearchfield] = useState([]);
+
   const [count, setCount] = useState(0);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -25,14 +31,18 @@ function App() {
       .then((users) => {
         setRobots(users);
       });
-    console.log(count);
-  }, [count]); // only run if count changes
+  }, []); // only run if count changes
+
+  const dispatch = useDispatch();
+
+  const text = useSelector((state) => state.searchField);
 
   const onSearchChange = (event) => {
-    setSearchfield(event.target.value); // search field gets updated
+    dispatch(setSearchField(event.target.value)); // search field gets updated
   };
+
   const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    return robot.name.toLowerCase().includes(text.toLowerCase());
   });
   return !robots.length ? (
     <h1>Loading</h1>
@@ -48,6 +58,9 @@ function App() {
       </Scroll>
     </div>
   );
-}
+};
 
+// connect is a higher order function
+// returns another function
+//connect(mapStateToProps, mapDispatchToProps)
 export default App;
